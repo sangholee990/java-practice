68fe1ccc73e00f3557e1715e9fc11392e84bc713
sangholee990 <80809068+sangholee990@users.noreply.github.com>
2021-11-12 14:30:45 +0900

    Update IndepClassListener.java

--- 20211112/IndepClassListener.java
+++ 20211112/IndepClassListener.java
@@ -9,18 +9,18 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class IndepClassListener extends JFrame {
-public IndepClassListener() {
-setTitle(":Action 이벤트 리스너 예제");
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-Container c = getContentPane();
-c.setLayout(new FlowLayout());
-JButton btn = new JButton("Action");
-btn.addActionListener(new MyActionListener()); // Action 리스너 달기
-c.add(btn);
-setSize(350, 150);
-setVisible(true); 
+	public IndepClassListener() {
+		setTitle(":Action 이벤트 리스너 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+		btn.addActionListener(new MyActionListener()); // Action 리스너 달기
+		c.add(btn);
+		setSize(350, 150);
+		setVisible(true);
 
-}
+	}
 
 	public static void main(String[] args) {
 		new IndepClassListener();
